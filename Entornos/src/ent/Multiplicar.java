bfdb5d13ac7b3cb7258b629fd5d97a6f33f9918f
iluntze <iluntze@iluntze>
2018-04-06 11:29:43 +0200

    Primera Subida

---
+++ Entornos/src/ent/Multiplicar.java
@@ -0,0 +1,35 @@
+
+		package ent;
+
+		import java.util.*;
+
+		public class Multiplicar {
+			
+			private int num1, num2,resultado;
+			Scanner teclado = new Scanner(System.in);
+			
+			public void numeros(){
+				System.out.println("Introduce un número");
+				num1 = teclado.nextInt();
+				System.out.println("Introduce un número");
+				num2 = teclado.nextInt();
+			}
+
+			public void sumar(){
+				resultado = num1*num2;
+				System.out.println("El resultado de multiplicar " + num1 + " con " +num2 + " es: " + resultado );
+			}
+			
+			public static void main (String[] arg){
+				
+				Multiplicar obj = new Multiplicar();
+				
+				obj.numeros();
+				obj.sumar();
+				
+			}
+		
+
+	}
+
+
